e973977b5973edc6282160ae7aa3c9da34bcef6e
syed zohaib <zohaibsyed914@gmail.com>
2025-11-19 18:05:26 +0500

    changes

--- index.js
+++ index.js
@@ -1,47 +1,7 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import connectDB from "./src/config/connectDB.js";
-import userRoutes from './src/routes/auth/userRoutes.js'
-import quotationRoutes from './src/routes/quotation/quotationRoutes.js'
-import cookieParser from "cookie-parser";
+import app from "./server.js";
 
-dotenv.config();
-
-const app = express();
-
-// Middlewares
-app.use(
-    cors({
-        origin: [
-            'http://localhost:5173',
-            'https://dynamic-quotation-system-client.vercel.app/',
-            '*'
-        ],
-        credentials: true,
-        methods: ['GET', "POST", "PUT", "DELETE"],
-        allowedHeaders: ["content-type", "Authorization"],
-    })
-)
-app.use(express.json());
-app.use(cookieParser());
-
-// Database Connect
-connectDB();
-
-// Basic Route  
-app.get("/", (req, res) => {
-    res.send("API is running...");
-});
-
-app.use("/api/v1/auth", userRoutes);
-app.use("/api/v1/quotation", quotationRoutes);
-
-
-
-// Server Start
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
---
+++ server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./src/config/connectDB.js";
+import userRoutes from "./src/routes/auth/userRoutes.js";
+import quotationRoutes from "./src/routes/quotation/quotationRoutes.js";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+    cors({
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+    })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+connectDB();
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+ 
+app.use("/api/v1/auth", userRoutes);
+app.use("/api/v1/quotation", quotationRoutes);
+
+export default app;
